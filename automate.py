399c58bd9c028ba1350ed7854c15699fa173c52d
Ian McKechnie <imckechn@uoguelph.ca>
2023-03-08 10:18:20 -0500

    wrote code to handle upto ten VMs in the conf file. Took out code from the azureVmBuilder as that's not the approach I will be taking

--- automate.py
+++ automate.py
@@ -1,21 +1,37 @@
 import configparser
-import azure
-
+from azureVmBuilder import *
 
 # Read the config files
 azureConfig = configparser.ConfigParser()
 gcpConfig = configparser.ConfigParser()
 
-azureConf = "azure.conf"
-gcpConf = "gcp.conf"
+azureConf = "confFiles/azure.conf"
+gcpConf = "confFiles/gcp.conf"
 
 azureConfig.read(azureConf)
 gcpConfig.read(gcpConf)
 
 #aws_access_key_id = config['default']['aws_access_key_id']
 
+awsVMs = []
+for i in range(10):
+    try:
+        ans = buildVMs(azureConfig['azure0' + str(i)])
+        awsVMs.append(ans)
+
+    except:
+        break
+
+#Now check for a tenth VM since it's naming convention changes
+try:
+    ans = buildVMs(azureConfig['azure10'])
+    awsVMs.append(ans)
+except:
+    pass
+
+# GCP
 
 # Azure
-azureVMs = []
-azureVMs.append(azureConf['azure01'])
-azureVMs.append(azureConf['azure02'])
+# azureVMs = []
+# azureVMs.append(azureConfig['azure01'])
+# azureVMs.append(azureConfig['azure02'])
---
+++ azureVmBuilder.py
@@ -0,0 +1,7 @@
+# Import the needed credential and management objects from the libraries.
+import os
+
+def buildVMs(config):
+    print(config)
+    
+    
