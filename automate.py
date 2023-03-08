f7668b8efcda253412a33f7d7a9c910f0888a5db
Ian McKechnie <imckechn@uoguelph.ca>
2023-03-08 11:03:45 -0500

    Can now build Azure VMs

--- automate.py
+++ automate.py
@@ -13,13 +13,47 @@ gcpConfig.read(gcpConf)
 
 #aws_access_key_id = config['default']['aws_access_key_id']
 
-awsVMs = []
-for i in range(10):
-    try:
-        ans = buildVMs(azureConfig['azure0' + str(i)])
-        awsVMs.append(ans)
+#Get the resource grous
+os.system("az group create --name images --location canadacentral")
 
-    except:
+resourceGroups = []
+for i in range(1, 10):
+    try:
+        group = azureConfig['azure0' + str(i)]['resource-group']
+
+        if group not in resourceGroups:
+            resourceGroups.append(group)
+            os.system("az group create --name " + group + " --location canadacentral")
+
+    except Exception as e:
+        break
+
+#Now check for a tenth VM since it's naming convention changes
+try:
+    group = azureConfig['azure0' + str(i)]['resource-group']
+
+    if group not in resourceGroups:
+        resourceGroups.append(group)
+        os.system("az group create --name " + group + " --location canadacentral")
+except:
+    pass
+
+
+#Now create the VMS
+awsVMs = []
+for i in range(1, 10):
+    try:
+        confData = azureConfig['azure0' + str(i)]
+        ans = buildVMs(i, confData)
+
+        if not ans:
+            print("Error building VM #", i)
+            break
+        else:
+            awsVMs.append(ans)
+
+    except Exception as e:
+        print("Found ", i - 1, " VMs")
         break
 
 #Now check for a tenth VM since it's naming convention changes
