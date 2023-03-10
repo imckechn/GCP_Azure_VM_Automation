54535e7f0db96a555a33fb023f7c84109e6fcd7c
Ian McKechnie <imckechn@uoguelph.ca>
2023-03-09 21:25:41 -0500

    Making progress in getting the GCP part

--- automate.py
+++ automate.py
@@ -25,61 +25,63 @@ if os.path.isfile(gcpConf):
 
 #aws_access_key_id = config['default']['aws_access_key_id']
 
-if awsExists:
-    #Get the resource grous
-    os.system("az group create --name images --location canadacentral")
+# if awsExists:
+#     #Get the resource grous
+#     os.system("az group create --name images --location canadacentral")
 
-    resourceGroups = []
-    for i in range(1, 10):
-        try:
-            group = azureConfig['azure0' + str(i)]['resource-group']
+#     resourceGroups = []
+#     for i in range(1, 10):
+#         try:
+#             group = azureConfig['azure0' + str(i)]['resource-group']
 
-            if group not in resourceGroups:
-                resourceGroups.append(group)
-                os.system("az group create --name " + group + " --location canadacentral")
+#             if group not in resourceGroups:
+#                 resourceGroups.append(group)
+#                 os.system("az group create --name " + group + " --location canadacentral")
 
-        except Exception as e:
-            break
+#         except Exception as e:
+#             break
 
-    #Now check for a tenth VM since it's naming convention changes
-    try:
-        group = azureConfig['azure0' + str(i)]['resource-group']
+#     #Now check for a tenth VM since it's naming convention changes
+#     try:
+#         group = azureConfig['azure0' + str(i)]['resource-group']
 
-        if group not in resourceGroups:
-            resourceGroups.append(group)
-            os.system("az group create --name " + group + " --location canadacentral")
-    except:
-        pass
+#         if group not in resourceGroups:
+#             resourceGroups.append(group)
+#             os.system("az group create --name " + group + " --location canadacentral")
+#     except:
+#         pass
 
 
-    #Now create the VMS
-    awsVMs = []
-    for i in range(1, 10):
-        try:
-            confData = azureConfig['azure0' + str(i)]
-            ans = azureBuildVMs(i, confData)
+#     #Now create the VMS
+#     awsVMs = []
+#     for i in range(1, 10):
+#         try:
+#             confData = azureConfig['azure0' + str(i)]
+#             ans = azureBuildVMs(i, confData)
 
-            if not ans:
-                print("Error building VM #", i)
-                break
-            else:
-                awsVMs.append(ans)
+#             if not ans:
+#                 print("Error building VM #", i)
+#                 break
+#             else:
+#                 awsVMs.append(ans)
 
-        except Exception as e:
-            print("Found ", i - 1, " VMs")
-            break
+#         except Exception as e:
+#             print("Found ", i - 1, " VMs")
+#             break
 
-    #Now check for a tenth VM since it's naming convention changes
-    try:
-        ans = azureBuildVMs(azureConfig['azure10'])
-        awsVMs.append(ans)
-    except:
-        pass
+#     #Now check for a tenth VM since it's naming convention changes
+#     try:
+#         ans = azureBuildVMs(azureConfig['azure10'])
+#         awsVMs.append(ans)
+#     except:
+#         pass
 
 # GCP
 
 #Set the project id
+print("Setting project id to ", PROJECT_ID)
 os.system("gcloud config set project " + PROJECT_ID)
+print("Done")
 
 if gcpExists:
     #Now create the VMS
--- gcpVmBuilder.py
+++ gcpVmBuilder.py
@@ -17,15 +17,39 @@ def gcpBuildVMs(vmNum, config):
         data.append(config['purpose'])
         data.append(config['os'])
 
+        print("Name: ", name)
+        print("Image: ", image)
+        print("Image Project: ", imageProject)
+        print("Zone: ", zone)
+        print("\n\n")
+
+        print("Running it")
+        # print(
+        #     "gcloud compute instances create " + name +
+        #     " --image=" + image +
+        #     " --image-project=" + imageProject +
+        #     " --zone=" + zone +
+        #     " --machine-type=e2-medium" +
+        #     " --boot-disk-size=10"
+        # )
         os.system("gcloud compute instances create " + name +
-                        " --machine-type " + image +
-                        " --image-family " + imageProject +
-                        "--image-project " + imageProject +
-                        " --zone " + zone
+                        " --image=" + image +
+                        " --image-project=" + imageProject +
+                        " --zone=" + zone +
+                        " --machine-type=e2-medium" +
+                        " --boot-disk-size=10"
         )
 
         return data
 
     except Exception as e:
-        print("Azure VM #", vmNum, " is bad")
+        print("GCP VM #", vmNum, " is bad")
         return False
+
+# gcloud compute instances create linuxserver01 
+# --image=debian-10-buster-v20210916 
+# --image-project=debian-cloud 
+# --zone=northamerica-northeast2-a 
+# --machine-type=e2-medium 
+# --boot-disk-size=10 
+
